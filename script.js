a53d11a1204694788838c6dfd79b68a84de02089
sprite <ponkaew.wasin@gmail.com>
2021-11-03 20:33:26 -0500

    added grid system

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 let container = document.querySelector('.container');
+//let row = container.querySelectorAll('.row_grid');
+let row = document.getElementsByClassName('row_grid')
+let cell = document.querySelectorAll('col_grid')
 
+function make_rows(num_rows){
+    for(i = 0; i < num_rows; i ++){
+        let row_div = document.createElement('div');
+        row_div.classList.add('row_grid');
+        container.append(row_div);
+    }
+    console.log(row.length)
+}
+
+
+function make_col(num_col){
+    for(i=0;i<row.length; i++){
+        for(j=0;j<num_col; j++){
+            let col_div = document.createElement('div');
+            col_div.classList.add('col_grid');
+            row[j].append(col_div);
+        }
+    }
+}
+
+
+
+make_rows(16);
+make_col(16);
