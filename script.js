ee24cb8e14d3e2568a00618b5e4ef7c20f9ac4a2
sprite <ponkaew.wasin@gmail.com>
2021-11-05 19:27:19 -0500

    added sketch

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
+
+function change(e){
+    e.target.style.backgroundColor = 'red';
+}
 
 // Creating rows and columns to form grid
 function make_rows(num_rows){
@@ -9,19 +13,19 @@ function make_rows(num_rows){
     }
 }
 
-
 function make_col(num_col){
-    let rows = container.querySelectorAll('.row_grid');
-    rows.forEach((row,i) => {
+    const rows = container.querySelectorAll('.row_grid');
+    rows.forEach((row) => {
         for(j=0;j<num_col; j++){
             let col_div = document.createElement('div');
             col_div.classList.add('col_grid');
+            col_div.addEventListener('mouseover', change); 
             row.append(col_div);
         }
     })
 }
 
-
+let current = 0;
 function change_size(num){
     // Clears any existing grid first
     while (container.firstChild){
@@ -29,15 +33,20 @@ function change_size(num){
     }
     make_rows(num);
     make_col(num);
+    current = num;
 }
 
+const cells = document.querySelectorAll('col_grid');
+const clear_btn = document.querySelector('#clear');
+clear_btn.addEventListener('click', clear);
 change_size(25);
 
-let cell = document.querySelector('.col_grid');
 
-cell.addEventListener('mouseenter', e =>{
-    e.style.backgroundColor = 'orange';
-    container.append(cell);
-});
+
+function clear(){
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'black';
+    });
+}
 
 
